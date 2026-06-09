ba980269ebd32839f33fb09ead5ae649e00f0c7d
gretakasongo <gretakasongo07@gmail.com>
2026-06-09 11:05:19 +0200

    button declaration splashscreen

--- app/src/main/java/com/example/weeklyweatherapp/SplashActivity.kt
+++ app/src/main/java/com/example/weeklyweatherapp/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.example.weeklyweatherapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,10 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+        // Button declarations
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
